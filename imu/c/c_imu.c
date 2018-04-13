78b652d60a4866a665421310a1e495c4aa21886d
Zachary Weeden <zdw7287@rit.edu>
2018-04-12 20:25:40 -0400

    Refactored C portion of IMU

--- imu/c/c_imu.c
+++ imu/c/c_imu.c
@@ -18,7 +18,7 @@
 
 long long int get_current_us(void){
     struct timeval timer_usec; 
-    long long int timestamp_usec; /* timestamp in microsecond */
+    long long int timestamp_usec;  // epoch in microseconds
     if (!gettimeofday(&timer_usec, NULL)) {
         timestamp_usec = ((long long int) timer_usec.tv_sec) * 1000000ll + (long long int) timer_usec.tv_usec;
     }
@@ -30,10 +30,11 @@ long long int get_current_us(void){
 
 float turn_loop(float degrees_to_turn, float bias){
     int time_elapsed = 0;
+    float delta_time_seconds = 0.0;
+    float raw_imu_reading = 0.0;
     float degrees_traveled = 0.0;
     long long int prev_proc_time = 0;
-    float dt = 0.0;
-    float imu_reading = 0.0;
+    float dps_reading_minus_bias = 0.0
 
     // MAPPING
     int fd = open("/dev/mem", O_RDWR | O_SYNC);
@@ -41,29 +42,26 @@ float turn_loop(float degrees_to_turn, float bias){
     void* imu = map + (ADDR_IMU & MAP_MASK);
     void* wz_dps = imu + WZ_DPS_OFFSET;
 
-    // while we've been turning for less than 1 second
-    while (time_elapsed <= 5000){
-        // skip the first 1ms can't integrate single point
-        if (time_elapsed != 0){
+    while (time_elapsed <= 1000){  // while we've been turning for less than 1 second
+        if (time_elapsed != 0){  // skip the first 1ms can't integrate single point
             // riemann sum rectangles
-            dt = (get_current_us() - prev_proc_time)/1000000.0;
-            imu_reading = *((uint32_t*)wz_dps);
-            printf("%.6f   %.6f\n", imu_reading, imu_reading-bias);
-            if (imu_reading >  32767){ 
-                imu_reading = -1 * (65536 - imu_reading);
+            delta_time_seconds = (get_current_us() - prev_proc_time)/1000000.0;
+            raw_imu_reading = *((uint32_t*)wz_dps);
+            if (raw_imu_reading >  32767){ 
+                raw_imu_reading = -1 * (65536 - raw_imu_reading);
             }
-            degrees_traveled += ((imu_reading/65.536) * dt);
+            dps_reading_minus_bias = (raw_imu_reading/65.536) - bias
+            degrees_traveled += (dps_reading_minus_bias * delta_time_seconds);
             printf("%.6f\n", degrees_traveled);
         }
-        // determine if 90 degrees reached
-        if (degrees_traveled >= degrees_to_turn){
+        if (degrees_traveled >= degrees_to_turn){  // determine if 90 degrees reached
             break;
         }
         time_elapsed++;
         prev_proc_time = get_current_us();
         usleep(ONE_MS_IN_US);  // 1ms sleep
     }
-    printf("bias: %.6f\n", bias);
+
     // CLOSE MAP - RETURN VALUE
     munmap(map, MAP_SIZE);
     close(fd);
