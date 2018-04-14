809d5787dcddf1906929a3871760c08d0d461a06
Zachary Weeden <zdw7287@rit.edu>
2018-04-14 16:10:12 -0400

    Properly handling/converting ticks to cm

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -2,18 +2,18 @@ import time
 from ctypes import cdll, c_float, POINTER
 
 SOUND_CM_PER_SECOND = 34000
-MICROSECONDS_IN_SECOND = 1000000.
+SYSTEM_CLOCK_HZ = 100000000.  # 100MHz
 
 raw_input('Ensure to write bitstream before running this program!')
 class Sensor:
     def __init__(self):
         self.c_sensor = cdll.LoadLibrary('./sensor.so')
-        self.c_sensor.fetch_echo_results.restype = POINTER(c_float * 3)  # array of floats (ticks or microseconds?)
+        self.c_sensor.fetch_echo_results.restype = POINTER(c_float * 3)
         self.c_sensor.enable_all_sensors()
 
     def read(self):
-        sensor_readings = self.c_sensor.fetch_echo_results()
-        sensor_cms = map(lambda reading: (SOUND_CM_PER_SECOND * ((reading / 2.) / MICROSECONDS_IN_SECOND)), sensor_readings)
+        sensor_ticks = self.c_sensor.fetch_echo_results()
+        sensor_cms = list(map(lambda round_trip_tick_reading: (SOUND_CM_PER_SECOND * ((round_trip_tick_reading / SYSTEM_CLOCK_HZ) / 2.)), sensor_ticks))
         left_sensor_cm = sensor_cms[0]
         middle_sensor_cm = sensor_cms[1]
         right_sensor_cm = sensor_cms[2]     
