4b8f6e5a871de3bb6363b12a77d4861acbdc9a65
Zachary Weeden <zdw7287@rit.edu>
2018-04-18 17:11:03 -0400

    Wrapped duty cycle set in method

--- web_test/control_logic.py
+++ web_test/control_logic.py
@@ -14,6 +14,21 @@ class ControlLogic:
         self.imu = IMU(1)  # 1 second calibration
         self.motor = Motor()
 
+    def set_fifty_percent_duty_cycle(self):
+        """
+        Method to set period of pwm to 10ms and at a 50% duty cycle
+        TODO: Coupled motors may need to be inverted if set in backward direction
+        ie. @75% forward set 750000, backward set 250000 
+        """
+        self.motor.set_period_a(1000000.)  # 1000000 ticks - 10ms
+        self.motor.set_period_b(1000000.)  # 1000000 ticks - 10ms
+        self.motor.set_period_c(1000000.)  # 1000000 ticks - 10ms
+        self.motor.set_period_d(1000000.)  # 1000000 ticks - 10ms
+        self.motor.set_duty_a(500000.)  # 500000 ticks - 5ms
+        self.motor.set_duty_b(500000.)  # 500000 ticks - 5ms
+        self.motor.set_duty_c(500000.)  # 500000 ticks - 5ms
+        self.motor.set_duty_d(500000.)  # 500000 ticks - 5ms
+
     def set_command(self, command_string):
         """
         Setter method to clearly set class attributes
@@ -22,61 +37,31 @@ class ControlLogic:
         # parse command received from web interface
         if command_string == 'straight':
             self.motor.set_motors_forward()
-            self.motor.set_period_a(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_b(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_c(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_d(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_duty_a(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_b(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_c(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_d(500000.)  # 500000 ticks - 5ms
+            self.set_fifty_percent_duty_cycle()
+            self.motor.enable_motors()
 
         elif command_string == 'backward':
             self.motor.set_motors_backward()
-            self.motor.set_period_a(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_b(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_c(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_d(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_duty_a(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_b(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_c(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_d(500000.)  # 500000 ticks - 5ms
+            self.set_fifty_percent_duty_cycle()
+            self.motor.enable_motors()
 
         elif command_string == 'left':
             self.motor.set_motors_for_left_turn()
-            self.motor.set_period_a(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_b(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_c(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_d(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_duty_a(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_b(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_c(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_d(500000.)  # 500000 ticks - 5ms
+            self.set_fifty_percent_duty_cycle()
+            self.motor.enable_motors()
+            self.imu.get_degrees_turned(90.)
 
         elif command_string == 'right':
             self.motor.set_motors_for_right_turn()
-            self.motor.set_period_a(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_b(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_c(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_d(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_duty_a(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_b(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_c(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_d(500000.)  # 500000 ticks - 5ms
+            self.set_fifty_percent_duty_cycle()
+            self.motor.enable_motors()
+            self.imu.get_degrees_turned(-90.)
 
         elif command_string == '360':
             self.motor.set_motors_for_left_turn()
-            self.motor.set_period_a(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_b(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_c(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_period_d(1000000.)  # 1000000 ticks - 10ms
-            self.motor.set_duty_a(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_b(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_c(500000.)  # 500000 ticks - 5ms
-            self.motor.set_duty_d(500000.)  # 500000 ticks - 5ms
+            self.set_fifty_percent_duty_cycle()
+            self.motor.enable_motors()
             self.imu.get_degrees_turned(360.)
             
-
-        self.motor.enable_motors()
         else:
             raise Exception('Unable to parse the command received from the web interface')
