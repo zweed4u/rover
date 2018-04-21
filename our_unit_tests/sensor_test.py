5d7a7d9bfc63e7c7dd799915516ccd785ae5c929
Zachary Weeden <zdw7287@rit.edu>
2018-04-21 16:24:31 -0400

    Added unit test directory for tests on our firmware, modified web interface backend logic and added individual sensor disables

---
+++ our_unit_tests/sensor_test.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+import time
+from sensor import Sensor
+
+
+class SensorTest(object):
+    def __init__(self):
+        self.sensors = Sensor()
+
+    def enable_all(self):
+        self.sensors.enable_all_sensors()
+
+    def disable_all(self):
+        self.sensors.disable_sensors()
+
+    def fetch_all(self):
+        return self.sensors.read_all()
+
+
+class SensorA(SensorTest):
+    def __init__(self):
+        super(SensorA, self).__init__()
+
+    def enable_a(self):
+        self.sensors.enable_sensor_a()
+
+    def disable_a(self):
+        self.sensors.disable_sensor_a()
+
+    def fetch_a(self):
+        return self.sensors.read_sensor_a()
+
+
+class SensorB(SensorTest):
+    def __init__(self):
+        super(SensorB, self).__init__()
+
+    def enable_b(self):
+        self.sensors.enable_sensor_b()
+
+    def disable_b(self):
+        self.sensors.disable_sensor_b()
+
+    def fetch_b(self):
+        return self.sensors.read_sensor_b()
+
+
+class SensorC(SensorTest):
+    def __init__(self):
+        super(SensorC, self).__init__()
+
+    def enable_c(self):
+        self.sensors.enable_sensor_c()
+
+    def disable_c(self):
+        self.sensors.disable_sensor_c()
+
+    def fetch_c(self):
+        return self.sensors.read_sensor_c()
+
+
+testA = SensorA()
+testB = SensorB()
+testC = SensorC()
+
+testA.enable_a()
+# testB.enable_b()
+# testC.enable_c()
+while 1:
+    print testA.fetch_a()
+    # print testB.fetch_a()
+    # print testC.fetch_a()
+    time.sleep(2)
--- web_test/control_logic.py
+++ web_test/control_logic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # Zachary Weeden 2018
 # Rover web interface backend
-
+import time
 from imu import IMU
 from motor import Motor
 
@@ -13,21 +13,15 @@ class ControlLogic:
         """
         self.imu = IMU(1)  # 1 second calibration
         self.motor = Motor()
-
-    def set_fifty_percent_duty_cycle(self):
-        """
-        Method to set period of pwm to 10ms and at a 50% duty cycle
-        TODO: Coupled motors may need to be inverted if set in backward direction
-        ie. @75% forward set 750000, backward set 250000 
-        """
-        self.motor.set_period_a(1000000.)  # 1000000 ticks - 10ms
-        self.motor.set_period_b(1000000.)  # 1000000 ticks - 10ms
-        self.motor.set_period_c(1000000.)  # 1000000 ticks - 10ms
-        self.motor.set_period_d(1000000.)  # 1000000 ticks - 10ms
         self.motor.set_duty_a(500000.)  # 500000 ticks - 5ms
         self.motor.set_duty_b(500000.)  # 500000 ticks - 5ms
         self.motor.set_duty_c(500000.)  # 500000 ticks - 5ms
         self.motor.set_duty_d(500000.)  # 500000 ticks - 5ms
+        self.motor.set_period_a(1000000.)  # 1000000 ticks - 10ms
+        self.motor.set_period_b(1000000.)  # 1000000 ticks - 10ms
+        self.motor.set_period_c(1000000.)  # 1000000 ticks - 10ms
+        self.motor.set_period_d(1000000.)  # 1000000 ticks - 10ms
+
 
     def set_command(self, command_string):
         """
@@ -37,31 +31,33 @@ class ControlLogic:
         # parse command received from web interface
         if command_string == 'straight':
             self.motor.set_motors_forward()
-            self.set_fifty_percent_duty_cycle()
             self.motor.enable_motors()
+            time.sleep(2)
+            self.motor.disable_motor()
 
         elif command_string == 'backward':
             self.motor.set_motors_backward()
-            self.set_fifty_percent_duty_cycle()
             self.motor.enable_motors()
+            time.sleep(2)
+            self.motor.disable_motor()
 
         elif command_string == 'left':
             self.motor.set_motors_for_left_turn()
-            self.set_fifty_percent_duty_cycle()
             self.motor.enable_motors()
             self.imu.get_degrees_turned(90.)
+            self.motor.disable_motor()
 
         elif command_string == 'right':
             self.motor.set_motors_for_right_turn()
-            self.set_fifty_percent_duty_cycle()
             self.motor.enable_motors()
             self.imu.get_degrees_turned(-90.)
+            self.motor.disable_motor()
 
         elif command_string == '360':
             self.motor.set_motors_for_left_turn()
-            self.set_fifty_percent_duty_cycle()
             self.motor.enable_motors()
             self.imu.get_degrees_turned(360.)
+            self.motor.disable_motor()
             
         else:
             raise Exception('Unable to parse the command received from the web interface')
