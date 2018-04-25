4bec6d1f7623f745dba95b8b0dfca6be3198c8fd
Zachary Weeden <zdw7287@rit.edu>
2018-04-25 14:39:56 -0400

    Added getters for acceleration for x, y, z and added getters for dps for x, y

--- test_suite/C/c_imu.c
+++ test_suite/C/c_imu.c
@@ -11,9 +11,9 @@
 #include <sys/time.h>
 
 #define ADDR_IMU 0x43C20000
-#define AX_DPS_OFFSET 0
-#define AY_DPS_OFFSET 4
-#define AZ_DPS_OFFSET 8
+#define AX_G_OFFSET 0
+#define AY_G_OFFSET 4
+#define AZ_G_OFFSET 8
 #define WX_DPS_OFFSET 12
 #define WY_DPS_OFFSET 16
 #define WZ_DPS_OFFSET 20
@@ -28,6 +28,81 @@ unsigned long int get_current_us(void){
     struct timeval tv;
     gettimeofday(&tv,NULL);
     return 1000000 * tv.tv_sec + tv.tv_usec;
+}
+
+double get_current_ax_g(void){
+    signed int raw_imu_reading = 0;
+    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    void* map = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, ADDR_IMU & ~MAP_MASK);
+    void* imu = map + (ADDR_IMU & MAP_MASK);
+    void* ax_g = imu + AX_G_OFFSET;
+    raw_imu_reading = *((uint32_t*)ax_g);
+    if (raw_imu_reading >  32767){ 
+        raw_imu_reading = -1 * (65536 - raw_imu_reading);
+    }
+    munmap(map, MAP_SIZE);
+    close(fd);
+    return (raw_imu_reading/16384.0);
+}
+
+double get_current_ay_g(void){
+    signed int raw_imu_reading = 0;
+    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    void* map = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, ADDR_IMU & ~MAP_MASK);
+    void* imu = map + (ADDR_IMU & MAP_MASK);
+    void* ay_g = imu + AY_G_OFFSET;
+    raw_imu_reading = *((uint32_t*)ay_g);
+    if (raw_imu_reading >  32767){ 
+        raw_imu_reading = -1 * (65536 - raw_imu_reading);
+    }
+    munmap(map, MAP_SIZE);
+    close(fd);
+    return (raw_imu_reading/16384.0);
+}
+
+double get_current_az_g(void){
+    signed int raw_imu_reading = 0;
+    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    void* map = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, ADDR_IMU & ~MAP_MASK);
+    void* imu = map + (ADDR_IMU & MAP_MASK);
+    void* az_g = imu + AZ_G_OFFSET;
+    raw_imu_reading = *((uint32_t*)az_g);
+    if (raw_imu_reading >  32767){ 
+        raw_imu_reading = -1 * (65536 - raw_imu_reading);
+    }
+    munmap(map, MAP_SIZE);
+    close(fd);
+    return (raw_imu_reading/16384.0);
+}
+
+double get_current_wx_dps(void){
+    signed int raw_imu_reading = 0;
+    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    void* map = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, ADDR_IMU & ~MAP_MASK);
+    void* imu = map + (ADDR_IMU & MAP_MASK);
+    void* wx_dps = imu + WX_DPS_OFFSET;
+    raw_imu_reading = *((uint32_t*)wx_dps);
+    if (raw_imu_reading >  32767){ 
+        raw_imu_reading = -1 * (65536 - raw_imu_reading);
+    }
+    munmap(map, MAP_SIZE);
+    close(fd);
+    return (raw_imu_reading/65.536);
+}
+
+double get_current_wy_dps(void){
+    signed int raw_imu_reading = 0;
+    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    void* map = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, ADDR_IMU & ~MAP_MASK);
+    void* imu = map + (ADDR_IMU & MAP_MASK);
+    void* wy_dps = imu + WY_DPS_OFFSET;
+    raw_imu_reading = *((uint32_t*)wy_dps);
+    if (raw_imu_reading >  32767){ 
+        raw_imu_reading = -1 * (65536 - raw_imu_reading);
+    }
+    munmap(map, MAP_SIZE);
+    close(fd);
+    return (raw_imu_reading/65.536);
 }
 
 double get_current_wz_dps(void){
